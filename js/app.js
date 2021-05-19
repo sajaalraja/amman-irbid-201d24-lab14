7ab2f2250e272b796486ae20b7c2a8163237e3ad
sajaalraja <sajamajed611@yahoo.com>
2021-05-19 23:35:01 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 'use strict';
 
+
+
 // Cart constructor.
-const Cart = function(items) {
+
+const Cart = function(items) { ///////////////////////////
   // this.items is an array of CartItem instances.
   this.items = items;
+  
+
+  Cart.allcart.push(this);
+
+  
 };
 
-Cart.prototype.addItem = function(product, quantity) {
+Cart.allcart = []; // array
+
+
+Cart.prototype.addItem = function(product, quantity) { // function to add items 
+  
+  
+  this.items = new CartItem(product, quantity);
+
+  // console.log(Cart.allcart);
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+
 };
 
 Cart.prototype.saveToLocalStorage = function() {
+
+
+  
+  let data = JSON.stringify(CartItem.allItems);
+  localStorage.setItem('cart', data);  
+
+
+
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
 };
 
@@ -19,13 +44,22 @@ Cart.prototype.removeItem = function(item) {
   // Note: You will have to decide what kind of parameter to pass in here!
 };
 
-const CartItem = function(product, quantity) {
+
+const CartItem = function(product, quantity) { //////////////////////
   this.product = product;
   this.quantity = quantity;
+
+
+
+  CartItem.allItems.push(this);
+
+  // console.log(CartItem.allItems);
 };
 
+CartItem.allItems= [];
+
 // Product contructor.
-const Product = function(filePath, name) {
+const Product = function(filePath, name) { ///////////////////
   this.filePath = filePath;
   this.name = name;
   Product.allProducts.push(this);
--- js/catalog.js
+++ js/catalog.js
@@ -9,41 +9,105 @@ const cart = new Cart([]);
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
+  let oldData = JSON.parse(localStorage.getItem('cart'));
+  if(oldData !== undefined & oldData !== null){
+    CartItem.allItems = oldData;
+
+    updateCounter();
+    updateCartPreview();
+    
+  }
+  
+  
+  
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
+  let optionEl;
   for (let i in Product.allProducts) {
+    optionEl = document.createElement('option');
+    optionEl.textContent = Product.allProducts[i].name;
 
+    selectElement.appendChild(optionEl);
   }
 
 }
+let a,b;
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
 
+  event.preventDefault();
+  // TODO: Prevent the page from reloading
+  a = event.target.items.value ;
+  b = event.target.quantity.value; 
+  // console.log(event.target.items.value); ///////////////////////////////////////
+
+  // let quantityNumber = event.
+
+  
   // Do all the things ...
-  addSelectedItemToCart();
+  addSelectedItemToCart(a, b);
   cart.saveToLocalStorage();
+
+
   updateCounter();
   updateCartPreview();
 
 }
 
+
+// console.log(cart);
 // TODO: Add the selected item and quantity to the cart
-function addSelectedItemToCart() {
+function addSelectedItemToCart(a,b) {
+
+  cart.addItem(a,b);
+
+
+  
+  // console.log(cart);
+
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  let name = document.getElementById('itemCount');
+
+  if(CartItem.allItems.length !== 0){
+    name.textContent = `(${CartItem.allItems.length})`;
+  }
+}
+
+let ulEl = document.createElement('ul');
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
+  let cartContents = document.getElementById('cartContents');
+  
+  ulEl.textContent='';
+  
+  cartContents.appendChild(ulEl);
+  
+  let liEl;
+  for(let i=0;i<CartItem.allItems.length;i++){
+    liEl = document.createElement('li');
+    ulEl.appendChild(liEl);
+
+    liEl.textContent = `${CartItem.allItems[i].quantity} : ${CartItem.allItems[i].product} `;
+  }
+  
+
+  
+  
+
+  
+
+
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
 }
@@ -57,3 +121,4 @@ catalogForm.addEventListener('submit', handleSubmit);
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
 populateForm();
+
